68ba18cf003c5b14c71cb829da44de899641759d
Benjamin Fox <benjaminf@intergen.co.nz>
2016-04-22 11:52:48 +1200

    Updating cargo documentation

--- implementors/iron/middleware/trait.Handler.js
+++ implementors/iron/middleware/trait.Handler.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['iron_pipeline'] = ["impl <a class='trait' href='http://ironframework.io/core/iron/iron/middleware/trait.Handler.html' title='iron::middleware::Handler'>Handler</a> for <a class='struct' href='iron_pipeline/struct.Pipeline.html' title='iron_pipeline::Pipeline'>Pipeline</a>",];
+implementors['iron_pipeline'] = ["impl <a class='trait' href='http://ironframework.io/core/iron/iron/middleware/trait.Handler.html' title='iron::middleware::Handler'>Handler</a> for <a class='struct' href='iron_pipeline/struct.Pipeline.html' title='iron_pipeline::Pipeline'>Pipeline</a>",];implementors['iron_pipeline'] = ["impl <a class='trait' href='http://ironframework.io/core/iron/iron/middleware/trait.Handler.html' title='iron::middleware::Handler'>Handler</a> for <a class='struct' href='iron_pipeline/struct.Pipeline.html' title='iron_pipeline::Pipeline'>Pipeline</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
