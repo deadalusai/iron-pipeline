89e3aed0c89a46191d8aad1531c210dc7a03e661
Benjamin Fox <deadalus.ai@gmail.com>
2016-05-07 21:57:10 +1200

    Code formatting. Doc text wrapping

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -2,9 +2,10 @@
 //!
 //! # A note on `iron::middleware::Handler`
 //!
-//! All types which implement `iron::middleware::Handler` can also be used as iron-pipeline middleware.
-//! However because the `Handler` trait does not understand the concept of "next" middleware,
-//! it is generally only useful to put such handlers at the _end_ of a pipeline.
+//! All types which implement `iron::middleware::Handler` can also be used
+//! as iron-pipeline middleware. However because the `Handler` trait does
+//! not understand the concept of "next" middleware, it is generally only
+//! useful to put such handlers at the _end_ of a pipeline.
 
 pub mod fork;
 pub mod handle;
